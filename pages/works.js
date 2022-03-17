7afe6260ab94bdd1467296c6260f6e75f5c09d94
andika <andikaherup@gmail.com>
2022-03-17 22:01:53 +0700

    This is a new commit for what I originally planned to be amended

---
+++ pages/works.js
@@ -0,0 +1,36 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumblocardweb from '../public/images/works/locardweb.jpg'
+import thumblocardadmin from '../public/images/works/locardadmin.jpg'
+
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+                </Heading>
+                <SimpleGrid columns={[ 1,1,2 ]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="locardweb" title="Locardweb" thumbnail={thumblocardweb}>
+                            A Mini marketplace for local business
+
+                            (   Probably website has been taken down by the company.)
+                            </WorkGridItem>
+
+                    </Section>
+                    <Section>
+                    <WorkGridItem id="locardadmin" title="Locardadmin" thumbnail={thumblocardadmin}>
+                            An admin panel for locardweb
+
+                            (  Probably website has been taken down by the company)
+                            </WorkGridItem>
+
+                    </Section>
+                </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
